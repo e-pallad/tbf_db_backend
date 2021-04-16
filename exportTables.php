b2e252d8bc5c3b8d14ab707c2739a95a20e89c20
Eric Pallad <e.pallad@pp-itservices.de>
2021-04-16 15:08:08 +0200

    fixed typo

--- exportTables.php
+++ exportTables.php
@@ -14,7 +14,7 @@
     
     if ($table == 'RI-TBF_SEF_Apparateliste') {
         $query = "SELECT `PnPID`,`TBF_ID`,`R&I EB68-Nr.`,`Feld-Nr.`,`Zchn. Rev. Nr.`,`Zustand/Bearbeitung`,`Bemerkung`,`AKZ_Gr1_Standort`,`AKZ_Gr2_Anlagenteil`,`AKZ_Gr3_Aggregat`,`AKZ_Gr4_Nummer`,`AKZ_Gr5_Aggregat`,`AKZ_Gr6_Nummer`,`Benennung`,`Benennung Zusatz`,`Hersteller`,`Typ`,`Medium`,`Nennleistung`,`Nennspannung`,`Nennstrom`,`Fördervolumen`,`Drehzahl`,`max. zul. Druck`,`max. zul. Temperatur`,`Volumen`,`Fläche`,`Gewicht`,`Werkstoff`,`Bauart`,`Zugehörige Sicherheitseinrichtung` FROM `Gesamtdatenbank` WHERE `TableID` = 1";
-        $fields = [18, 19, 20, 22, 23, 24, 25, 26, 27];
+        $korrFields = [18, 19, 20, 22, 23, 24, 25, 26, 27];
     } elseif ($table == 'RI-TBF_SEF_Armaturenliste') {
         $query = "SELECT `PnPID`,`TBF_ID`,`R&I EB68-Nr.`,`Feld-Nr.`,`Zchn. Rev. Nr.`,`Zustand/Bearbeitung`,`Bemerkung`,`AKZ_Gr1_Standort`,`AKZ_Gr2_Anlagenteil`,`AKZ_Gr3_Aggregat`,`AKZ_Gr4_Nummer`,`AKZ_Gr5_Aggregat`,`AKZ_Gr6_Nummer`,`Benennung`,`Benennung Zusatz`,`Hersteller`,`Typ`,`Medium`,`Nennleistung`,`Nennspannung`,`Nennstrom`,`Fördervolumen`,`Drehzahl`,`max. zul. Druck`,`max. zul. Temperatur`,`Volumen`,`Fläche`,`Gewicht`,`Werkstoff`,`Bauart`,`Zugehörige Sicherheitseinrichtung`,`DN`,`PN`,`NW`,`TBV/ITD Nr.`,`Einbauort bzw. Rohrleitungs Nr.` FROM `Gesamtdatenbank` WHERE `TableID` = 2";
     } elseif ($table == 'RI-TBF_SEF_Messstellenliste') {
@@ -93,7 +93,7 @@
 			
 			header('Content-Type: application/json;');
             header('Access-Control-Allow-Origin: *');
-            
+
             echo json_encode($data);
             break;
         default:
