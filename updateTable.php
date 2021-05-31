911bb71840b22118c1784a4e93cc484e1c39f0a2
Eric Pallad <e.pallad@pp-itservices.de>
2021-05-31 20:48:45 +0200

    reassign value after regex

--- updateTable.php
+++ updateTable.php
@@ -107,7 +107,7 @@
                     if (!empty($value) || $value === "0") {
                         // Check for comma seperated float values and convert to dot seperated if so
                         if (preg_match('/([0-9]+.{1,}),([0-9]{0,2})/', $value, $number)) {
-                            $numbervalue = str_replace('.', '', $number[1]) . "." . $number[3];
+                            $value = str_replace('.', '', $number[1]) . "." . $number[2];
                         }
                         /*
                         if (is_numeric($number)) {
