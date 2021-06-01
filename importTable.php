8b4dc4868c0491332d744024a12591b5b395100f
Eric Pallad <e.pallad@pp-itservices.de>
2021-06-01 21:10:33 +0200

    reset NULL in tableID

--- importTable.php
+++ importTable.php
@@ -38,6 +38,8 @@
         $tableID = 5;
     } elseif ($table == 'RI-TBF_SEF_Stoffstromliste') {
         $tableID = 6;
+    } else {
+        $tableID = NULL;
     }
     
     if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
