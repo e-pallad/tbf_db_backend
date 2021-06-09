05bdfae835b9231f656c0a9cab3cc06b3e03ebb1
Eric Pallad <e.pallad@pp-itservices.de>
2021-06-09 16:23:10 +0200

    removed uneccassary entries from lsit

--- backup/listMysqlBackups.php
+++ backup/listMysqlBackups.php
@@ -1,4 +1,5 @@
 <?php
-    $backupList = scandir(getcwd());
+    // https://stackoverflow.com/a/15774702
+    $backupList = array_diff(scandir(getcwd()), array('.', '..', 'backupMysql.php', 'listMysqlBackups.php', 'restoreMysql.php'));
     echo json_encode($backupList);
 ?>
