5dec1ee13afe8acd94db5f6b80f218546374a752
Eric Pallad <e.pallad@pp-itservices.de>
2021-07-19 01:55:55 +0200

    test

--- backup/listMysqlBackups.php
+++ backup/listMysqlBackups.php
@@ -1,8 +1,8 @@
 <?php
     // https://stackoverflow.com/a/15774702
     $backupList = array_diff(scandir(getcwd()), array('.', '..', 'backupMysql.php', 'listMysqlBackups.php', 'restoreMysql.php', 'index.php'));
-    arsort($backupList);
-    
+    asort($backupList);
+
     header("Content-type: application/pdf");
     header('Access-Control-Allow-Origin: *');
     header('Content-Disposition: attachment; filename="SEF-EVerbraucherliste.pdf";');
