0e3f05524a8029f4aabda891916490f0ea21d18f
Eric Pallad <e.pallad@pp-itservices.de>
2021-07-19 01:14:39 +0200

    set header to return values

--- backup/listMysqlBackups.php
+++ backup/listMysqlBackups.php
@@ -1,5 +1,10 @@
 <?php
     // https://stackoverflow.com/a/15774702
     $backupList = array_diff(scandir(getcwd()), array('.', '..', 'backupMysql.php', 'listMysqlBackups.php', 'restoreMysql.php'));
-    echo json_encode($backupList);
+
+    header("Content-type: application/pdf");
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Disposition: attachment; filename="SEF-EVerbraucherliste.pdf";');
+
+    json_encode($backupList);
 ?>
