a68365a5f158600ad6bf8bc43fd8dcc4242a0fd5
Eric Pallad <e.pallad@pp-itservices.de>
2021-07-19 01:51:16 +0200

    added sorting and excluding index.php

--- backup/listMysqlBackups.php
+++ backup/listMysqlBackups.php
@@ -1,6 +1,6 @@
 <?php
     // https://stackoverflow.com/a/15774702
-    $backupList = array_diff(scandir(getcwd()), array('.', '..', 'backupMysql.php', 'listMysqlBackups.php', 'restoreMysql.php'));
+    $backupList = asort(array_diff(scandir(getcwd()), array('.', '..', 'backupMysql.php', 'listMysqlBackups.php', 'restoreMysql.php', 'index.php')));
 
     header("Content-type: application/pdf");
     header('Access-Control-Allow-Origin: *');
