6f43e276854f901ebec61e0e9d194bd8a34f6996
Eric Pallad <e.pallad@pp-itservices.de>
2021-07-19 02:30:16 +0200

    added script to download files

---
+++ backup/exportBackup.php
@@ -0,0 +1,26 @@
+<?php
+    $method = $_SERVER['REQUEST_METHOD'];
+    $file = $_GET['file'];
+
+    switch ($method) {
+        case 'GET':
+            if (file_exists($file)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename="'.basename($file).'"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($file));
+                readfile('./' + $file);
+                exit;
+            } else {
+                echo http_response_code(403);
+                break;
+            }
+            break;
+        default:
+            echo http_response_code(403);
+            break;
+    }
+?>
