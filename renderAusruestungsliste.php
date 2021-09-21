3b26240ede8c177f72ea4f76554daa3ed7ec7a77
Eric Pallad <e.pallad@pp-itservices.de>
2021-09-21 18:06:57 +0200

    fixing width the 2nd

--- renderAusruestungsliste.php
+++ renderAusruestungsliste.php
@@ -48,9 +48,9 @@
                 if ($col == "AKZ Kodierung" || $col == "Benennung" || $col == "Benennung Zusatz") {
                     $this->Cell(35,10,$col,1,0,'C');
                 } elseif ($col == "Kenndaten 1" || $col == "Kenndaten 2" || $col == "Kenndaten 3" || $col == "Kenndaten 4" || $col == "R&I EB68-Nr.") {
-                    $this->Cell(12,10,$col,1,0,'C');
+                    $this->Cell(20,10,$col,1,0,'C');
                 } elseif ($col == "TBV/ITD Nr." || $col == "Feld-Nr.") {
-                    $this->Cell(8,10,$col,1,0,'C');
+                    $this->Cell(12,10,$col,1,0,'C');
                 } elseif ($col == "Einbauort bzw. Rohrleitungs Nr.") {
                     $x=$this->GetX();
                     $y=$this->GetY();
