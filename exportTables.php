b565691210dc4a4705ef11fded15369b1fd0c9e6
Eric Pallad <e.pallad@pp-itservices.de>
2022-07-15 16:14:24 +0200

    finished debug

--- exportTables.php
+++ exportTables.php
@@ -97,7 +97,6 @@
 
     switch ($method) {
         case 'GET':
-            /*
             $xlsx = SimpleXLSXGen::fromArray( $CSVdata );
             
             header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8');
@@ -105,13 +104,13 @@
             header('Content-Disposition: attachment; filename="'. $table .'.xlsx";');
 
             $xlsx->downloadAs("$table.xlsx");
-			*/
-            
+			
+            /*
 			header('Content-Type: application/json;');
             header('Access-Control-Allow-Origin: *');
 
             echo json_encode($CSVdata);
-            
+            */
             break;
         default:
             echo http_response_code(403);
